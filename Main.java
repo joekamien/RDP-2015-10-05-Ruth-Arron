662aa2ec27314d9bf042b8a6224a651ee81a7d43
joekamien <joekamien@users.noreply.github.com>
2015-10-05 12:09:38 -0700

    Add regular expression to match expected input.

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner (System.in); 
+		int numberOfPairs = Integer.parseInt(scanner.nextLine());
+		for(int i = 0; i < numberOfPairs; i++){
+			//TODO input validation
+			String s = scanner.nextLine();
+			//extract integers and create new pair
+			Pattern p = Pattern.compile("\\(\\d+,\\d+\\)");
+			Matcher m = p.matcher(s);
+			if(m.matches()){
+				System.out.println("Matched");
+			}
+			else{
+				System.out.println("did not match");
+			}
+			//Pair pair = new Pair(Integer.parseInt(m.group(0)), Integer.parseInt(m.group(1)));
+			//System.out.println(pair);
+		}
+		scanner.close();
 	}
-
+	
 }
